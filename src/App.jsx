72667f046945ca1c454742ebc550b4faf4d8894f
RadimYassin <radimyassin20@gamil.com>
2023-03-04 23:17:42 +0100

    updting

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 
 import { Card } from './components/';
 import movies from './data.json';
-
+import "./styles/App.css"
 function App() {
   return (
     <main>
       {movies.map(movie => (
         <Card classes="mr" key={`${movie.id}`}>
-           <Card.Body> hello</Card.Body>
+           <Card.Img src={movie.image} alt={movie.title}/>
+           <Card.Body>
+              <Card.Title>{movie.title}</Card.Title>
+              <Card.Text>{movie.desc}</Card.Text>
+              <Card.Btn>{movie.ctaText}</Card.Btn>
+           </Card.Body>
         </Card>
       ))}
     </main>
